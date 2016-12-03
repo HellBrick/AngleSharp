7cc4b4f7b8b4705a7bb53003e3bc2abc2ebd8e9f
HellBrick <hellbrick@outlook.com>
2016-12-03 19:20:23 +0300

    [debug] Playground project + Enumeration test

---
+++ src/AngleSharp.Performance.Playground/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+
+namespace AngleSharp.Performance.Playground
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			HtmlParser parser = new HtmlParser();
+
+			for ( int i = 0; i < 1000; i++ )
+			{
+				using ( IHtmlDocument document = parser.ParseDocument( TestData.TorrentSite ) )
+				{
+					foreach ( IElement link in document.Links )
+					{
+					}
+
+					foreach ( IHtmlScriptElement script in document.Scripts )
+					{
+					}
+				}
+			}
+		}
+	}
+}
